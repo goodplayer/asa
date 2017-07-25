19adf5b696746358802dab4f0ea48c001def3067
goodplayer <meidomx@gmail.com>
2017-07-26 07:26:18 +0800

    fastcgi backend

--- demo/backend/backend.go
+++ demo/backend/backend.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"net/http/fcgi"
 )
 
 type DefaultHttpHandler struct {
@@ -14,9 +16,41 @@ func (this *DefaultHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	w.Write([]byte(fmt.Sprintln("hello world!")))
 }
 
+type DefaultFcgiHttpHandler struct {
+}
+
+func (this *DefaultFcgiHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Header)
+	w.Header().Add("Content-Type", "text/plain")
+	w.Write([]byte(fmt.Sprintln("hello world of fcgi!")))
+}
+
 func main() {
-	err := http.ListenAndServe("0.0.0.0:8888", new(DefaultHttpHandler))
-	if err != nil {
-		panic(err)
-	}
+	ch := make(chan bool, 2)
+
+	go func() {
+		err := http.ListenAndServe("0.0.0.0:8888", new(DefaultHttpHandler))
+		if err != nil {
+			panic(err)
+		}
+		ch <- true
+	}()
+
+	go func() {
+		addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:8887")
+		if err != nil {
+			panic(err)
+		}
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			panic(err)
+		}
+		err = fcgi.Serve(addr, new(DefaultFcgiHttpHandler))
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	<-ch
+	<-ch
 }
