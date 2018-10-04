88352f4f3b9c1ae66a3a4fb476f35c49c827c752
goodplayer <woshiyigelaji@gmail.com>
2018-10-04 23:26:28 +0800

    pickRandom

--- core/loadbalancer/doc/CONCEPTS.md
+++ core/loadbalancer/doc/CONCEPTS.md
@@ -4,18 +4,20 @@
 3. look-aside
 
 ####pick:
-1. round-robin
-	weight rr
-2. random
-	weight random
-3. least
-	connection
-	latency
-	traffic
-4. fixed connection
-	hash
-	consistency hash
-	same connection
+##### 1. round-robin
+* rr
+* weight rr
+##### 2. random
+* random
+* weight random
+##### 3. least
+* connection
+* latency
+* traffic
+##### 4. fixed connection
+* hash
+* consistency hash
+* same connection
 
 
 | - | non-weight | weight | sort |
--- core/loadbalancer/normal.go
+++ core/loadbalancer/normal.go
@@ -1,19 +1,70 @@
 package loadbalancer
 
-import "github.com/goodplayer/asa/api"
+import (
+	"errors"
+	"math/rand"
+	"sync"
+
+	"github.com/goodplayer/asa/api"
+)
+
+const (
+	_SUB_ARRAY_SHIFT = 8
+	_SUB_ARRAY_MASK  = 2 ^ _SUB_ARRAY_SHIFT - 1
+)
 
 type LoadBalancer struct {
 	strategy api.PickStrategy
+
+	data    [][]*api.Endpoint
+	mapping map[int]int
+	cnt     int
+	lock    sync.RWMutex
+}
+
+func NewLoadBalancer(strategy api.PickStrategy) api.LoadBalancer {
+	lb := new(LoadBalancer)
+	lb.strategy = strategy
+	lb.cnt = 0
+	lb.mapping = make(map[int]int)
+	//TODO
+	return lb
 }
 
 func (this *LoadBalancer) Pick(key interface{}) (*api.Endpoint, error) {
 	switch this.strategy {
 	case api.RANDOM:
-		//TODO
+		return this.pickRandom()
 	case api.ROUND_ROBIN:
 		//TODO
 	case api.FIXED_KEY:
 		//TODO
 	}
 	panic("implement me")
+}
+
+func twoIdx(idx int) (int, int) {
+	return idx >> _SUB_ARRAY_SHIFT, idx & _SUB_ARRAY_MASK
+}
+
+func (this *LoadBalancer) Add(key interface{}) {
+	//TODO
+}
+
+func (this *LoadBalancer) Remove(key interface{}) {
+	//TODO
+}
+
+func (this *LoadBalancer) pickRandom() (*api.Endpoint, error) {
+	this.lock.RLock()
+	cnt := this.cnt
+	if cnt <= 0 {
+		this.lock.RUnlock()
+		return nil, errors.New("no element found")
+	}
+	idx := rand.Intn(cnt)
+	firstIdx, secondIdx := twoIdx(idx)
+	endpoint := this.data[firstIdx][secondIdx]
+	this.lock.RUnlock()
+	return endpoint, nil
 }
